18056aa374c0c169fe6f228da7bdbc7684f92b0b
Axel-J-L <70752905+Axel-J-L@users.noreply.github.com>
2020-10-03 19:41:43 +0200

    menus are super wonky at the moment but nothing is broken have to move the rest of the stuff into DartController

--- {code}pendent/src/DartController.java
+++ {code}pendent/src/DartController.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class DartController{
     private Menus menus;
     private Input input = Input.getInstance(); // DO singleton
@@ -19,19 +21,49 @@ public class DartController{
         System.out.println(menus.EOL + menus.DIVIDER + menus.EOL + "     Good Bye!"+menus.EOL+menus.DIVIDER);
         System.exit(0);
     }
+
+    public boolean authenticate(String password) { // authenticates passwords
+        String enteredPassword = input.getInput("Enter your password: ");
+        return password.equals(enteredPassword);
+    }
+
+    public void authManager() { // Why is menu auth?
+        String password = "admin1234";
+        boolean authSuccess = authenticate(password);
+
+        if (authSuccess) {
+            managerMenu();
+        } else {
+            System.out.println(menus.EOL+"*** Wrong password *** "+menus.EOL);
+            mainMenu();
+        }
+    }
+    public void authEmployee() {
+        String password = "password123";
+
+        Boolean authSuccess = authenticate(password);
+
+        if (authSuccess) {
+            employeeMenu();
+        } else {
+            System.out.println(menus.EOL+"*** Wrong password ***"+menus.EOL);
+            mainMenu();
+        }
+    }
+
     public void mainMenu(){
         menus.mainMenu();
         do {
             String choice = Input.input.nextLine();
             switch (choice) {
                 case "1":
-                    menus.managerMenu();
+                    managerMenu();
                     break;
                 case "2":
-                    menus.employeeMenu();
+                    employeeMenu();
                     break;
                 case "3":
-                    menus.customerMenu();
+                    customerMenu();
                     break;
                 case "4":
                     exit();
@@ -45,6 +77,8 @@ public class DartController{
     }
 
     public void managerMenu() {
+        menus.managerMenu();
+        System.out.print(menus.PROMPT);
         do {
             String choice = input.getInput("");
             switch (choice) {
@@ -76,9 +110,291 @@ public class DartController{
             }
         } while (true);
     }
+
+    public void employeeMenu(){
+        menus.employeeMenu();
+        System.out.print(menus.PROMPT);
+        do {
+            String choice = Input.input.nextLine();
+            switch (choice) {
+                case "1":
+                    empGameOptions();
+                    break;
+                case "2":
+                    empAlbumOptions();
+                    break;
+                case "3":
+                    empCustomerOptions();
+                    break;
+                case "4":
+                    System.out.println("\n>> Total Rent Profit");
+                    System.out.println("TODO!");
+                    employeeMenu();
+                    break;
+                case "5":
+                    mainMenu();
+                    break;
+                default:
+                    System.out.println(menus.DIVIDER+"\n--- Invalid input ---");
+                    System.out.print(menus.PROMPT);
+                    break;
+            }
+        } while (true);
+    }
+    public void empGameOptions() {
+        menus.empGameOptions();
+        System.out.print(menus.PROMPT);
+        do {
+            String choice = Input.input.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println(menus.EOL+">> New Game");
+                    storage.addAlbum();
+                    input.userCheck();
+                    employeeMenu();
+                    break;
+                case "2":
+                    System.out.println(menus.EOL+">> Remove Game:");
+                    storage.viewAlbums();
+                    storage.removeAlbum();
+                    input.userCheck();
+                    employeeMenu();
+                    break;
+                case "3":
+                    System.out.println(menus.EOL+">> All Games");
+                    storage.viewAlbums();
+                    input.userCheck();
+                    employeeMenu();
+                    break;
+                case "4":
+                    employeeMenu();
+                    break;
+                default:
+                    System.out.println(menus.DIVIDER+menus.EOL+"--- Invalid input ---");
+                    System.out.print(menus.PROMPT);
+                    break;
+            }
+        } while (true);
+    }
+    public void empAlbumOptions() {
+        menus.empAlbumOptions();
+        System.out.print(menus.PROMPT);
+        do {
+            String choice = Input.input.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println(menus.EOL+">> New Album");
+                    storage.addAlbum();
+                    input.userCheck();
+                    employeeMenu();
+                    break;
+                case "2":
+                    System.out.println(menus.EOL+">> Remove Album:");
+                    storage.viewAlbums();
+                    storage.removeAlbum();
+                    input.userCheck();
+                    employeeMenu();
+                    break;
+                case "3":
+                    System.out.println(menus.EOL+">> All Albums");
+                    storage.viewAlbums();
+                    input.userCheck();
+                    employeeMenu();
+                    break;
+                case "4":
+                    employeeMenu();
+                    break;
+                default:
+                    System.out.println(menus.DIVIDER+menus.EOL+"--- Invalid input ---");
+                    System.out.print(menus.PROMPT);
+                    break;
+            }
+        } while (true);
+    }
+    public void empCustomerOptions() {
+        menus.empCustomerOptions();
+        System.out.print(menus.PROMPT);
+        do {
+            String choice = Input.input.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println(menus.EOL+">> New Customer");
+                    storage.addCustomer();
+                    input.userCheck();
+                    employeeMenu();
+                    break;
+                case "2":
+                    System.out.println(menus.EOL+">> Remove Customer");
+                    storage.removeCustomer();
+                    input.userCheck();
+                    employeeMenu();
+                    break;
+                case "3":
+                    System.out.println(menus.EOL+">> All Customers");
+                    storage.viewCustomer();
+                    input.userCheck();
+                    employeeMenu();
+                    break;
+                case "4":
+                    employeeMenu();
+                    break;
+                default:
+                    System.out.println(menus.DIVIDER+menus.EOL+"--- Invalid input ---");
+                    System.out.print(menus.PROMPT);
+                    break;
+            }
+        } while (true);
+    }
+    public void customerMenu() {
+        menus.customerMenu();
+        System.out.print(menus.PROMPT);
+        do {
+            String choice = Input.input.nextLine();
+            switch (choice) {
+                case "1":
+                    gameOptions();
+                    input.userCheck();
+                    break;
+                case "2":
+                    albumOptions();
+                    input.userCheck();
+                    break;
+                case "3":
+                    System.out.println(menus.EOL+">> Add membership");
+                    // requestList = addMembership();
+                    customerMenu();
+                    input.userCheck();
+                    break;
+                case "4":
+                    System.out.println(menus.EOL+">> Upgrade membership");
+                    System.out.println("Method todo");
+                    break;
+                case "5":
+                    System.out.println(menus.EOL+">> Inbox");
+                    inboxMenu();
+                    input.userCheck();
+                    break;
+                case "6":
+                    mainMenu();
+                    break;
+                default:
+                    System.out.println(menus.DIVIDER+menus.EOL+"--- Invalid input ---");
+                    System.out.print(menus.PROMPT);
+                    break;
+            }
+        } while (true);
+    }
+    public void gameOptions() {
+        menus.gameOptions();
+        System.out.print(menus.PROMPT);
+        do {
+            String choice = Input.input.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println(menus.EOL+">> Rent Game");
+                    storage.rentGame();
+                    input.userCheck();
+                    customerMenu();
+                    break;
+                case "2":
+                    System.out.println(menus.EOL+">> Return Game");
+                    System.out.println("add the method for now returns you to main menu");
+                    customerMenu();
+                    input.userCheck();
+                    break;
+                case "3":
+                    System.out.println(menus.EOL+">> All Games");
+                    System.out.println("add the method for now returns you to main menu");
+                    customerMenu();
+                    input.userCheck();
+                    break;
+                case "4":
+                    System.out.println(menus.EOL+">> Search Games");
+                    System.out.println("add the method for now returns you to main menu");
+                    customerMenu();
+                    input.userCheck();
+                    break;
+                case "5":
+                    employeeMenu();
+                    input.userCheck();
+                    break;
+                default:
+                    System.out.println(menus.DIVIDER+menus.EOL+"--- Invalid input ---");
+                    System.out.print(menus.PROMPT);
+                    break;
+            }
+        } while (true);
+    }
+    public void albumOptions() {
+        menus.albumOptions();
+        System.out.print(menus.PROMPT);
+        do {
+            String choice = Input.input.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println(menus.EOL+">> Rent Album");
+                    storage.rentAlbum();
+                    employeeMenu();
+                    break;
+                case "2":
+                    System.out.println(menus.EOL+">> Return Album:");
+                    storage.viewAlbums();
+                    storage.returnAlbum();
+                    employeeMenu();
+                    break;
+                case "3":
+                    System.out.println(menus.EOL+">> All Albums");
+                    storage.viewAlbums();
+                    employeeMenu();
+                    break;
+                case "4":
+                    System.out.println(menus.EOL+">> Search Albums");
+                    storage.searchAlbums();
+                    employeeMenu();
+                    break;
+                case "5":
+                    employeeMenu();
+                    break;
+                default:
+                    System.out.println(menus.DIVIDER+menus.EOL+"--- Invalid input ---");
+                    System.out.print(menus.PROMPT);
+                    break;
+            }
+        } while (true);
+    }
+    public void inboxMenu() {
+        menus.inboxMenu();
+        System.out.print(menus.PROMPT);
+        do {
+            String choice = Input.input.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println(menus.EOL+">> View Messages");
+                    System.out.println("add the method for now returns you to main menu");
+                    mainMenu();
+                    break;
+                case "2":
+                    System.out.println(menus.EOL+">> Send Message");
+                    System.out.println("add the method for now returns you to main menu");
+                    mainMenu();
+                    break;
+                case "3":
+                    System.out.println(menus.EOL+">> Delete Message");
+                    System.out.println("add the method for now returns you to main menu");
+                    mainMenu();
+                    break;
+                case "4":
+                    mainMenu();
+                    break;
+                default:
+                    System.out.println(menus.DIVIDER+menus.EOL+"--- Invalid input ---");
+                    System.out.print(menus.PROMPT);
+                    break;
+            }
+        } while (true);
+    }
+}
+
     // Getters
 
     // Setters
-
-
-}
--- {code}pendent/src/Menus.java
+++ {code}pendent/src/Menus.java
@@ -21,21 +21,6 @@ public class Menus {
     public Menus() {
     }
 
-
-    private void exit() {
-        //close (public static final scanner (in Helper class))
-        Input.getInstance().tearDown();
-        System.out.println(EOL + DIVIDER + EOL + "     Good Bye!"+EOL+DIVIDER);
-        System.exit(0);
-    }
-
-    public boolean authenticate(String password) { // authenticates passwords
-        String enteredPassword = helper.getInput("Enter your password: ");
-        return password.equals(enteredPassword);
-    }
-
-    // TODO ---------------------------------------MAIN MENU--------------------------------------------------
-
     public void mainMenu() {
         String[] options = {" 1) Manager Menu", " 2) Employee Menu", " 3) Customer Menu", " 4) Exit System"};
         Menus menu = new Menus(EOL+"------Main-Menu------", options);
@@ -46,27 +31,12 @@ public class Menus {
         System.out.print(menu.PROMPT);
     }
 
-    // TODO ---------------------------------------MANAGER MENU--------------------------------------------------
-
     public void managerMenu() {
         String[] options = {" 1) Register Employee", " 2) Remove Employee", " 3) View Employees", " 4) Main Menu"};
         Menus menu = new Menus(EOL+"-----Manager-Menu-----", options);
         System.out.println(menu.title);
         for (int i = 0; i < options.length; i++) {
             System.out.println(options[i]);
-        }
-    }
-
-    // Move me daddy
-    public void authManager() { // Why is menu auth?
-        String password = "admin1234";
-        boolean authSuccess = authenticate(password);
-
-        if (authSuccess) {
-            managerMenu();
-        } else {
-            System.out.println(EOL+"*** Wrong password *** "+EOL);
-            mainMenu();
         }
     }
 
@@ -87,7 +57,6 @@ public class Menus {
         } requestList = null;
     }
 
-    // TODO ---------------------------------------EMPLOYEE MENU--------------------------------------------------
     public void employeeMenu() {
         if (requestList != null) {
             membershipRequestList();
@@ -98,86 +67,24 @@ public class Menus {
         for (int i = 0; i < options.length; i++) {
             System.out.println(options[i]);
         }
-        System.out.print(menu.PROMPT);
-        do {
-            String choice = Input.input.nextLine();
-            switch (choice) {
-                case "1":
-                    empGameOptions();
-                    break;
-                case "2":
-                    empAlbumOptions();
-                    break;
-                case "3":
-                    empCustomerOptions();
-                    break;
-                case "4":
-                    System.out.println("\n>> Total Rent Profit");
-                    System.out.println("TODO!");
-                    employeeMenu();
-                    break;
-                case "5":
-                    mainMenu();
-                    break;
-                default:
-                    System.out.println(DIVIDER+"\n--- Invalid input ---");
-                    System.out.print(menu.PROMPT);
-                    break;
-            }
-        } while (true);
     }
 
-    // TODO ---------------------------------------Employee SUBMENUS--------------------------------------------------
-
-    public void empGameOptions() {
+    public void empGameOptions () {
         String[] options = {" 1) Register Game", " 2) Remove Game", " 3) View Games", " 4) Employee Menu"};
-        Menus menu = new Menus(EOL+"-----Game-Options----", options);
+        Menus menu = new Menus(EOL + "-----Game-Options----", options);
         System.out.println(menu.title);
         for (int i = 0; i < options.length; i++) {
             System.out.println(options[i]);
         }
-        System.out.print(menu.PROMPT);
     }
 
     public void empAlbumOptions() {
         String[] options = {" 1) Register Album", " 2) Remove Album", " 3) View Albums", " 4) Employee Menu"};
-        Menus menu = new Menus(EOL+"----Album-Options----", options);
+        Menus menu = new Menus(EOL + "----Album-Options----", options);
         System.out.println(menu.title);
         for (int i = 0; i < options.length; i++) {
             System.out.println(options[i]);
         }
-        System.out.print(menu.PROMPT);
-        do {
-            String choice = Input.input.nextLine();
-            switch (choice) {
-                case "1":
-                    System.out.println(EOL+">> New Album");
-                    storage.addAlbum();
-                    helper.userCheck();
-                    employeeMenu();
-                    break;
-                case "2":
-                    System.out.println(EOL+">> Remove Album:");
-                    storage.viewAlbums();
-                    storage.removeAlbum();
-                    helper.userCheck();
-                    employeeMenu();
-                    break;
-                case "3":
-                    System.out.println(EOL+">> All Albums");
-                    storage.viewAlbums();
-                    helper.userCheck();
-                    employeeMenu();
-                    break;
-                case "4":
-                    employeeMenu();
-                    break;
-                default:
-                    System.out.println(DIVIDER+EOL+"--- Invalid input ---");
-                    System.out.print(menu.PROMPT);
-                    break;
-            }
-        } while (true);
     }
 
     public void empCustomerOptions() {
@@ -187,101 +94,16 @@ public class Menus {
         for (int i = 0; i < options.length; i++) {
             System.out.println(options[i]);
         }
-        System.out.print(menu.PROMPT);
-        do {
-            String choice = Input.input.nextLine();
-            switch (choice) {
-                case "1":
-                    System.out.println(EOL+">> New Customer");
-                    storage.addCustomer();
-                    helper.userCheck();
-                    employeeMenu();
-                    break;
-                case "2":
-                    System.out.println(EOL+">> Remove Customer");
-                    storage.removeCustomer();
-                    helper.userCheck();
-                    employeeMenu();
-                    break;
-                case "3":
-                    System.out.println(EOL+">> All Customers");
-                    storage.viewCustomer();
-                    helper.userCheck();
-                    employeeMenu();
-                    break;
-                case "4":
-                    employeeMenu();
-                    break;
-                default:
-                    System.out.println(DIVIDER+EOL+"--- Invalid input ---");
-                    System.out.print(menu.PROMPT);
-                    break;
-            }
-        } while (true);
     }
-
-    //-------------------------------------------------------------------------------------------------------
-
-    public void authEmployee() {
-        String password = "password123";
-
-        Boolean authSuccess = authenticate(password);
-
-        if (authSuccess) {
-            employeeMenu();
-        } else {
-            System.out.println(EOL+"*** Wrong password ***"+EOL);
-            mainMenu();
-        }
-    }
-
-    // TODO ---------------------------------------CUSTOMER MENU--------------------------------------------------
 
     public void customerMenu() {
         String[] options = {" 1) Game Options", " 2) Album Options", " 3) Add Membership", " 4) Upgrade Membership", " 5) Inbox", " 6) Main Menu"};
-        Menus menu = new Menus(EOL+"----Customer-Menu----", options);
+        Menus menu = new Menus(EOL + "----Customer-Menu----", options);
         System.out.println(menu.title);
         for (int i = 0; i < options.length; i++) {
             System.out.println(options[i]);
         }
-        System.out.print(menu.PROMPT);
-        do {
-            String choice = Input.input.nextLine();
-            switch (choice) {
-                case "1":
-                    gameOptions();
-                    helper.userCheck();
-                    break;
-                case "2":
-                    albumOptions();
-                    helper.userCheck();
-                    break;
-                case "3":
-                    System.out.println(EOL+">> Add membership");
-                    // requestList = addMembership();
-                    customerMenu();
-                    helper.userCheck();
-                    break;
-                case "4":
-                    System.out.println(EOL+">> Upgrade membership");
-                    System.out.println("Method todo");
-                    break;
-                case "5":
-                    System.out.println(EOL+">> Inbox");
-                    inboxMenu();
-                    helper.userCheck();
-                    break;
-                case "6":
-                    mainMenu();
-                    break;
-                default:
-                    System.out.println(DIVIDER+EOL+"--- Invalid input ---");
-                    System.out.print(menu.PROMPT);
-                    break;
-            }
-        } while (true);
     }
-// TODO ---------------------------------------CUSTOMER SUBMENUS--------------------------------------------------
 
     public void gameOptions() {
         String[] options = {" 1) Rent Game", " 2) Return Game", " 3) View Games", " 4) Search Games", " 5) Customer Menu"};
@@ -290,44 +112,6 @@ public class Menus {
         for (int i = 0; i < options.length; i++) {
             System.out.println(options[i]);
         }
-        System.out.print(menu.PROMPT);
-        do {
-            String choice = Input.input.nextLine();
-            switch (choice) {
-                case "1":
-                    System.out.println(EOL+">> Rent Game");
-                    storage.rentGame();
-                    helper.userCheck();
-                    customerMenu();
-                    break;
-                case "2":
-                    System.out.println(EOL+">> Return Game");
-                    System.out.println("add the method for now returns you to main menu");
-                    customerMenu();
-                    helper.userCheck();
-                    break;
-                case "3":
-                    System.out.println(EOL+">> All Games");
-                    System.out.println("add the method for now returns you to main menu");
-                    customerMenu();
-                    helper.userCheck();
-                    break;
-                case "4":
-                    System.out.println(EOL+">> Search Games");
-                    System.out.println("add the method for now returns you to main menu");
-                    customerMenu();
-                    helper.userCheck();
-                    break;
-                case "5":
-                    employeeMenu();
-                    helper.userCheck();
-                    break;
-                default:
-                    System.out.println(DIVIDER+EOL+"--- Invalid input ---");
-                    System.out.print(menu.PROMPT);
-                    break;
-            }
-        } while (true);
     }
 
     public void albumOptions() {
@@ -337,43 +121,7 @@ public class Menus {
         for (int i = 0; i < options.length; i++) {
             System.out.println(options[i]);
         }
-        System.out.print(menu.PROMPT);
-        do {
-            String choice = Input.input.nextLine();
-            switch (choice) {
-                case "1":
-                    System.out.println(EOL+">> Rent Album");
-                    storage.rentAlbum();
-                    employeeMenu();
-                    break;
-                case "2":
-                    System.out.println(EOL+">> Return Album:");
-                    storage.viewAlbums();
-                    storage.returnAlbum();
-                    employeeMenu();
-                    break;
-                case "3":
-                    System.out.println(EOL+">> All Albums");
-                    storage.viewAlbums();
-                    employeeMenu();
-                    break;
-                case "4":
-                    System.out.println(EOL+">> Search Albums");
-                    storage.searchAlbums();
-                    employeeMenu();
-                    break;
-                case "5":
-                    employeeMenu();
-                    break;
-                default:
-                    System.out.println(DIVIDER+EOL+"--- Invalid input ---");
-                    System.out.print(menu.PROMPT);
-                    break;
-            }
-        } while (true);
     }
-
-    // TODO ---------------------------------------Messenger/Inbox Menu--------------------------------------------------
 
     public void inboxMenu() {
         String[] options = {" 1) View messages", " 2) Send Message", " 3) Delete Message", " 4) Main Menu"};
@@ -382,34 +130,5 @@ public class Menus {
         for (int i = 0; i < options.length; i++) {
             System.out.println(options[i]);
         }
-        System.out.print(menu.PROMPT);
-        do {
-            String choice = Input.input.nextLine();
-            switch (choice) {
-                case "1":
-                    System.out.println(EOL+">> View Messages");
-                    System.out.println("add the method for now returns you to main menu");
-                    mainMenu();
-                    break;
-                case "2":
-                    System.out.println(EOL+">> Send Message");
-                    System.out.println("add the method for now returns you to main menu");
-                    mainMenu();
-                    break;
-                case "3":
-                    System.out.println(EOL+">> Delete Message");
-                    System.out.println("add the method for now returns you to main menu");
-                    mainMenu();
-                    break;
-                case "4":
-                    mainMenu();
-                    break;
-                default:
-                    System.out.println(DIVIDER+EOL+"--- Invalid input ---");
-                    System.out.print(menu.PROMPT);
-                    break;
-            }
-        } while (true);
     }
-
 }
